e77d592d6ef76fd7de104ab15cdcf76ad3f8ae14
Douglas DeVicariis <kootg04@gmail.com>
2021-03-14 22:28:18 -0700

    Another...

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := server.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	server.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
